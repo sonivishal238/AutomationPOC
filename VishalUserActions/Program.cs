b1c15a40772dbe44d6f1cca92f4c69d769758ebd
Vishal Soni <sonivishal238@gmail.com>
2024-06-05 17:17:53 +0530

    Added PR support text

--- VishalUserActions/Program.cs
+++ VishalUserActions/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Checking webhook
 // Checking webhook
-// Checking webhook
+// Checking webhook CMD PR test
 Console.WriteLine("Hello, World!");
